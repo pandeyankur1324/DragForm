1effa8c85f784253ff081ac2fe6916a80776d845
Ankur Pandey <pandeyankur55143@gmail.com>
2025-07-02 23:37:37 +0530

    authentication made compulsary

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,31 @@
 import Logo from "@/components/Logo";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
-function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
       <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
         <Logo />
         <div className="flex gap-4 items-center">
           <ThemeSwitcher />
-          <UserButton afterSignOutUrl="/sign-in" />
+          <SignedIn>
+            <UserButton afterSignOutUrl="/sign-in" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
         </div>
       </nav>
-      <main className="flex w-full flex-grow justify-center"> {children}</main>
+      <main className="flex w-full flex-grow justify-center">{children}</main>
     </div>
   );
 }
-
-export default Layout;
